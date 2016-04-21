002767576b02e15f17e06bbe94abf8ec05dc8bd2
maliang <295011445@qq.com>
2016-04-21 23:23:54 +0800

    ...

--- NetWorkStudy/NetWorkStudy/Section/mainPage/MainPageViewController.m
+++ NetWorkStudy/NetWorkStudy/Section/mainPage/MainPageViewController.m
@@ -21,8 +21,33 @@
     self.view.backgroundColor = [UIColor whiteColor];
     [self setTitle:@"网络请求学习"];
     [self addARowWithCellTitle:@"多个网络请求的同步问题" vcClass:[MoreRequestSyncViewController class] turnType:M_push];
+    [self groupSync];
     // Do any additional setup after loading the view.
 }
+
+- (void)groupSync
+{
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_group_enter(group);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+       
+        sleep(5);
+        NSLog(@"任务一完成");
+        dispatch_group_leave(group);
+    });
+    dispatch_group_enter(group);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+        sleep(8);
+        NSLog(@"任务二完成");
+        dispatch_group_leave(group);
+    });
+    dispatch_group_notify(group, dispatch_get_global_queue(0, 0), ^{
+        
+        NSLog(@"任务完成");
+    });
+}
+
 
 - (void)didReceiveMemoryWarning
 {
